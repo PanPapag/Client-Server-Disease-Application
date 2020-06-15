4b0add4c4300fffc9b4e3274d64c67f2d30105ba
PanPapag <pan_papageorgiou@hotmail.com>
2020-06-15 20:56:38 +0300

    Case 1 completed in client threads

--- client/src/io_utils.c
+++ client/src/io_utils.c
@@ -89,26 +89,46 @@ void *__client_threads_function(void *fl) {
 }
 
 void parse_query_file_and_create_threads(pthread_t *client_threads) {
+  char buffer[options.num_threads][MAX_BUFFER_SIZE];
   /* Get the number of lines of the query file */
-  int num_lines;
-  get_file_lines(options.query_file, &num_lines);
+  int num_queries;
+  get_file_lines(options.query_file, &num_queries);
   /*
     As the description mentions: "When all the threads are created, that is,
     we have one thread for each command of the file, then the threads should
     start all together to try to connect to whoServer and send their command.
 
     Given the statement above, we have to take care of 2 cases.
-    1. num_threads >= num_lines: We create just num_lines threads and nothing more
-    3. num_threads < num_lines: This case is the most complex one. To tackle
+    1. num_threads >= num_queries: We create just num_queries threads and nothing more
+    3. num_threads < num_queries: This case is the most complex one. To tackle
        this problem, when num_threads are fewer than the number of queries, we
-       are going to create (num_lines) div (num_threads) times exactly num_threads
+       are going to create (num_queries) div (num_threads) times exactly num_threads
        threads to serve num_threads queries per time. The last time we are going
-       to create just (num_lines) mod (num_threads) threads to serve the remaining
+       to create just (num_queries) mod (num_threads) threads to serve the remaining
        queries.
   */
-  if (options.num_threads >= num_lines) {
-    printf("CASE 1\n");
+  /* Open file for read only - handles binary fille too */
+  FILE *fp = fopen(options.query_file, "rb+");
+  /* Seperate the 2 cases */
+  if (options.num_threads >= num_queries) {
+    /* Initialize pthread barrier */
+    pthread_barrier_init(&barrier, NULL, num_queries);
+    for (size_t i = 0U; i < num_queries; ++i) {
+      /* Read query and store it to the buffer */
+      fgets(buffer[i], MAX_BUFFER_SIZE, fp);
+      /* Discard '\n' that fgets() stores */
+      buffer[i][strlen(buffer[i]) - 1] = '\0';
+      /* Create thread */
+      pthread_create(&client_threads[i], NULL, __client_threads_function, (void *)buffer[i]);
+    }
+    /* Wait all threads to finish and destroy barrier */
+    for (size_t i = 0U; i < num_queries; ++i) {
+      pthread_join(client_threads[i], NULL);
+    }
+		pthread_barrier_destroy(&barrier);
   } else {
     printf("CASE 2\n");
   }
+  /* Close file pointer */
+  fclose(fp);
 }
